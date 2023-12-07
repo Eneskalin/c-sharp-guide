d302b6374e70e9f2ad4368b62b90db4e9cbf0ad7
Eneskalin <enes6813@yandex.com>
2023-12-07 13:39:41 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿// See https://aka.ms/new-console-template for more information
+////int a = 5, b = 10;
+////Console.WriteLine(a + b);
+////Console.WriteLine(a * b);
+////Console.WriteLine(a / b);
+////// İlişkisel operatörler
+////Console.WriteLine(a < b);
+///Console.ReadKey();
+
+/*
+//// tek çift//
+//using System.ComponentModel.Design;
+
+////Console.WriteLine("Bir sayı giriniz:");
+////    int sayi = Convert.ToInt32(Console.ReadLine());
+////if (sayi%2==0)
+////{
+////    Console.WriteLine($"{sayi}çift bir sayıdır.");
+////    }
+////else
+////{
+////    Console.WriteLine($"{sayi} tek bir sayi.");
+////}
+
+
+////Console.WriteLine("Bir sayı giriniz:");
+////int n = Convert.ToInt32(Console.ReadLine());
+////if (n<0)
+////{
+////    Console.WriteLine($"|{n}|= {(n*-1)}");
+
+////}
+////else
+////{
+////    Console.WriteLine($"|{n}|= {n}");
+////}
+
+//var k= (char)Console.Read();
+//if (Char.IsDigit(k));
+//{ 
+//    Console.WriteLine("rakamdır");
+//}
+//else if (Char.IsLower(k)) 
+//{
+//    Console.WriteLine("Küçük Karakter")
+
+//}
+//else if (Char.IsUpper(k))
+//{
+//    Console.WriteLine("Büyük karakter");
+
+//}
+//else
+//{
+//    Console.WriteLine("Bilinmeyen Karakter");    
+//}
+
+//for (int i = 0; i < 10; i++)
+//{
+//	if (i%2==0)
+//	{
+//		Console.WriteLine($"{i} bir cift sayidir.");
+
+//	}
+//	else
+//	{
+//        Console.WriteLine($"{i} bir tek sayidir.");
+
+//    }
+
+
+//}
+
+/*int i = 0;
+while(i< 10)
+{
+    //Console.Write(i +" ");
+    //Console.Write("{0,-3}" ,i);
+    Console.WriteLine("{0,-3} {1,-3}" ,i , i^2);
+
+    i++;
+
+}
+int a =Convert.ToInt32(Console.ReadLine());
+int b =Convert.ToInt32(Console.ReadLine());
+
+for (int i = a; i < b; i++)
+    
+{
+    Console.WriteLine("{0,3}",i); //forr yazarsan azalan yaparsin
+
+}
+
+//diziler
+int[] numaralar;
+// int[] numaralar = new int[3] {3,4,5}; 
+// int[] numaralar = {3,4,5,6,7};
+numaralar = new int[3];
+numaralar[0] = 3;
+numaralar[1] = 4;
+numaralar[2] = 5;
+for (int i = 0; i < numaralar.Length; i++)
+{
+    Console.WriteLine(numaralar[i]);
+
+
+
+Console.WriteLine("Dizi boyutunu giriniz:");
+int boyut =Convert.ToInt32(boyut);
+Console.ReadKey();
+int[] sayilar = new int[boyut];
+var = ref= new Random();
+
+for (int i = 0; i < length; i++)
+{
+
+}
+*/
+int[] array ={2,3,4,5,};
+Console.WriteLine(array);
+
+
+}
+
+
+
